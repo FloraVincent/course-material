bb5402fcf71ff60e85f148e41a4009343d2f985a
FloraVincent <flora.j.vincent@gmail.com>
2014-09-22 15:30:17 +0200

    solution13

---
+++ exercices/013/Solution.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 22 15:12:14 2014
+
+@author: Flora Vincent
+"""
+import sys
+
+if len(sys.argv) == 1:
+    print("usage: python3 solution.py PARAM")
+else:
+    print(sys.argv[1])
