6fa00730244f1f66de970f7764696e7f59c87132
FloraVincent <flora.j.vincent@gmail.com>
2014-09-22 18:11:30 +0200

    add

---
+++ exercices/060/solution.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 22 18:04:50 2014
+
+@author: Flora Vincent
+"""
+for i in "abcdefghijklmnopqrstuvwyz":
+    for j in "abcdefghijklmnopqrstuvwyz":
+        print(i + j)
