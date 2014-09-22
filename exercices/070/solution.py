1ed926adfa44aff0df2243abdba18d22a0e1848d
FloraVincent <flora.j.vincent@gmail.com>
2014-09-22 18:18:05 +0200

    solution70

---
+++ exercices/070/solution.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 22 18:15:18 2014
+
+@author: Flora Vincent
+"""
+
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 22 18:04:50 2014
+
+@author: Flora Vincent
+"""
+for i in "abcdefghijklmnopqrstuvwyz":
+    for j in "abcdefghijklmnopqrstuvwyz":
+        if i != j:
+            print(i + j)
