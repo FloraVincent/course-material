b5da278e12f5d0322791154b33bd42f1108bc0a5
FloraVincent <flora.j.vincent@gmail.com>
2014-09-22 16:00:11 +0200

    solution14

---
+++ exercices/014/solution.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 22 15:50:01 2014
+
+@author: Flora Vincent
+"""
+
+import operator
+
+print(operator.__truediv__(245850922, 78256779))
+print(operator.__truediv__(operator.add(1, operator.pow(5, 0.5)), 2))
