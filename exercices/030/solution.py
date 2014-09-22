bf5ebc9b081700cd02673f9e35bd5558a6d2eab2
FloraVincent <flora.j.vincent@gmail.com>
2014-09-22 17:35:31 +0200

    solution

--- exercices/030/solution.py
+++ exercices/030/solution.py
@@ -6,6 +6,11 @@ Created on Mon Sep 22 16:33:21 2014
 """
 import operator
 
-for i in list(range(100)):
+a, i = 0, 0
+while i <= 100:
     if operator.mod(i, 2) == 0:
-        print(i)
+        a, i = a + i, i + 1
+    else:
+        i = i + 1
+print(a)
+    
---
+++ exercices/045/solution.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 22 17:24:38 2014
+
+@author: Flora Vincent
+"""
+import operator
+
+
+def sqrt(num):
+    print(operator.pow(num, 0.5))
