974d00b2fcb6e6abbd71b5b4e765d0f1cd4ce1b0
FloraVincent <flora.j.vincent@gmail.com>
2014-09-22 17:56:29 +0200

    sol14

--- exercices/050/solution.py
+++ exercices/050/solution.py
@@ -6,9 +6,10 @@ Created on Mon Sep 22 17:37:17 2014
 """
 import operator
 
+
 a, i = 0, 0
-while i <= 10:
-    if operator.mod(i, 3) == 0 | operator.mod(i, 5) == 0:
+while i < 100:
+    if (operator.mod(i, 3) == 0 or operator.mod(i, 5) == 0):
         a, i = a + i, i + 1
     else:
         i = i + 1
