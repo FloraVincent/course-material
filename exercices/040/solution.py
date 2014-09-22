634c3a565b6286bdb8e0981ea7004b2a879c3442
FloraVincent <flora.j.vincent@gmail.com>
2014-09-22 17:08:12 +0200

    solution40

---
+++ exercices/040/solution.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 22 16:33:21 2014
+
+@author: Flora Vincent
+"""
+import operator
+
+a, i = 0, 0
+while i <= 100:
+    if operator.mod(i, 2) == 0:
+        a, i = a + i, i + 1
+    else:
+        i = i + 1
+print(a)
