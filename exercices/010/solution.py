167e9a143a718e0638cb39f47495ccedc5dc1056
FloraVincent <flora.j.vincent@gmail.com>
2014-09-22 14:58:18 +0200

    sol10

---
+++ exercices/010/solution.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 22 14:53:21 2014
+
+@author: Flora Vincent
+"""
+
+for i in range(100):  # range starts at 0 and i want 100 displays
+    print("Hello World")
