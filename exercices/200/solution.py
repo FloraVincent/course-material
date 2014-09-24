60cbb5c09b04c2c0ba0c8e8fe4f0e22d3649d07b
FloraVincent <flora.j.vincent@gmail.com>
2014-09-24 10:12:18 +0200

    youyou

---
+++ exercices/200/solution.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep 24 09:37:26 2014
+
+@author: Flora Vincent
+"""
+# Define function
+
+
+def is_prime(num):
+    if num > 1:
+        for i in range(2, num):
+            if (num % i) == 0:
+                print("FALSE")
+            else:
+                print("TRUE")
+    else:
+        print("FALSE")
+    return
+
+
+# if num == int:
+    #  result = is_prime(num)
+    #  print(result)
+# else:
+    #  print("Please put an integer!")
